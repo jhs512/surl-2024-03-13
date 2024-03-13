0d819c86cfe9964f4a16bc37f23bb6f81259d4c2
jhs512 <jangka512@gmail.com>
2024-03-13 09:45:44 +0900

    work

---
+++ src/main/java/com/ll/surl20240313/domain/surl/surl/service/SurlService.java
@@ -0,0 +1,18 @@
+package com.ll.surl20240313.domain.surl.surl.service;
+
+import com.ll.surl20240313.domain.surl.surl.repository.SurlRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class SurlService {
+
+    private final SurlRepository surlRepository;
+
+    public long count() {
+        return surlRepository.count();
+    }
+}
