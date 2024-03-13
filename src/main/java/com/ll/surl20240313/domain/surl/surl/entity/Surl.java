af334e3f7ac493b868cd526ca44129b535fc0a44
jhs512 <jangka512@gmail.com>
2024-03-13 10:27:04 +0900

    work

--- src/main/java/com/ll/surl20240313/domain/surl/surl/entity/Surl.java
+++ src/main/java/com/ll/surl20240313/domain/surl/surl/entity/Surl.java
@@ -4,10 +4,17 @@ import com.ll.surl20240313.global.jpa.entity.BaseTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.*;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
+@Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
+@Getter
+@Setter
 public class Surl extends BaseTime {
     @Id
     @GeneratedValue(strategy = IDENTITY)
--- src/main/java/com/ll/surl20240313/domain/surl/surl/service/SurlService.java
+++ src/main/java/com/ll/surl20240313/domain/surl/surl/service/SurlService.java
@@ -1,9 +1,12 @@
 package com.ll.surl20240313.domain.surl.surl.service;
 
+import com.ll.surl20240313.domain.surl.surl.entity.Surl;
 import com.ll.surl20240313.domain.surl.surl.repository.SurlRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +17,21 @@ public class SurlService {
 
     public long count() {
         return surlRepository.count();
+    }
+
+    @Transactional
+    public Surl save(String url, String title) {
+        Surl surl = Surl.builder()
+                .url(url)
+                .title(title)
+                .build();
+
+        surlRepository.save(surl);
+
+        return surl;
+    }
+
+    public Optional<Surl> findById(long id) {
+        return surlRepository.findById(id);
     }
 }
