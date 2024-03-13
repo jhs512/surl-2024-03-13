c64cfaed6d06e6c00b32bf34a7a5fd3641c7ca9a
jhs512 <jangka512@gmail.com>
2024-03-13 11:24:17 +0900

    work

--- src/main/java/com/ll/surl20240313/domain/surl/surl/entity/Surl.java
+++ src/main/java/com/ll/surl20240313/domain/surl/surl/entity/Surl.java
@@ -1,9 +1,11 @@
 package com.ll.surl20240313.domain.surl.surl.entity;
 
+import com.ll.surl20240313.domain.member.member.entity.Member;
 import com.ll.surl20240313.global.jpa.entity.BaseTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.*;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
@@ -19,6 +21,8 @@ public class Surl extends BaseTime {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
+    @ManyToOne
+    private Member author;
     private String url;
     private String title;
 }
