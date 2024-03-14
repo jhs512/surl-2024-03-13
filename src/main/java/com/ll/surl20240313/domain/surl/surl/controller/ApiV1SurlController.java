8d3de911a4902fdae4bce5e945ac526b7e552df4
jhs512 <jangka512@gmail.com>
2024-03-14 09:19:46 +0900

    work

--- src/main/java/com/ll/surl20240313/domain/surl/surl/controller/ApiV1SurlController.java
+++ src/main/java/com/ll/surl20240313/domain/surl/surl/controller/ApiV1SurlController.java
@@ -3,11 +3,13 @@ package com.ll.surl20240313.domain.surl.surl.controller;
 import com.ll.surl20240313.domain.member.member.entity.Member;
 import com.ll.surl20240313.domain.member.member.service.MemberService;
 import com.ll.surl20240313.domain.surl.surl.service.SurlService;
+import com.ll.surl20240313.global.security.SecurityUser;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -32,7 +34,8 @@ public class ApiV1SurlController {
             @Valid @RequestBody SurlCreateReqBody reqBody,
             Principal principal
     ) {
-        log.debug("principal: {}", principal);
+        SecurityUser user = principal == null ? null : (SecurityUser) ((Authentication) principal).getPrincipal();
+        log.debug("user: {}", user);
         Member author = memberService.findById(4L).get();
         surlService.create(author, reqBody.url, reqBody.title);
     }
