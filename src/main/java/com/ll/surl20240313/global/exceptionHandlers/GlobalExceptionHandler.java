664dca30f14d98da35199f82287ae75f163e316e
jhs512 <jangka512@gmail.com>
2024-03-15 10:34:16 +0900

    work

--- src/main/java/com/ll/surl20240313/global/exceptionHandlers/GlobalExceptionHandler.java
+++ src/main/java/com/ll/surl20240313/global/exceptionHandlers/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import com.ll.surl20240313.standard.dto.Empty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,8 +37,15 @@ public class GlobalExceptionHandler {
     // 자연스럽게 발생시킨 예외처리
     private ResponseEntity<Object> handleApiException(Exception ex) {
         Map<String, Object> body = new LinkedHashMap<>();
-        body.put("resultCode", "500-1");
-        body.put("statusCode", 500);
+
+        if (ex instanceof AccessDeniedException) {
+            body.put("resultCode", "403-1");
+            body.put("statusCode", 403);
+        } else {
+            body.put("resultCode", "500-1");
+            body.put("statusCode", 500);
+        }
+
         body.put("msg", ex.getLocalizedMessage());
 
         LinkedHashMap<String, Object> data = new LinkedHashMap<>();
